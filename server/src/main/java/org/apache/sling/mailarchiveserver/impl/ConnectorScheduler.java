2e086bcff11d2d046be4e9c7895bf2ae6f79ebcc
Igor Bogomolov <igor.bogomolov@gmail.com>
2013-09-13 20:42:37 +0200

    #19: ExchangeConnector implemented

--- server/src/main/java/org/apache/sling/mailarchiveserver/impl/ConnectorScheduler.java
+++ server/src/main/java/org/apache/sling/mailarchiveserver/impl/ConnectorScheduler.java
@@ -5,25 +5,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.apache.felix.scr.annotations.ReferencePolicy;
 import org.apache.sling.mailarchiveserver.api.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
-public class ConnectorScheduler {
+public class ConnectorScheduler implements Runnable {
 
-	private static final int RETREIVE_MESSAGES_LIMIT = 3;
+	private static final int RETREIVE_MESSAGES_LIMIT = 10;
 	private static final Logger logger = LoggerFactory.getLogger(ConnectorScheduler.class);
 
+	@Reference(
+			cardinality=ReferenceCardinality.OPTIONAL_MULTIPLE, 
+			policy=ReferencePolicy.DYNAMIC,
+			referenceInterface=Connector.class)
 	private List<Connector> scheduledConnectors = new ArrayList<>();
 	private Deque<Connector> executionQueue;
-	
+
+	// TODO change?
 	@Activate
-	public void activate() {
-		// TODO check Bertran's example - sling.ScriptEngineManagerFactory
+	public void activate() throws InterruptedException {
+		new Thread(this).start();
+	}
+
+	@Override
+	public void run() {
 		while (true) {
 			logger.info("Checking new mail.");
 			Collections.sort(scheduledConnectors, null); // PROD comparator, introduce class abstract PriorityConnector implements Connector
@@ -31,11 +44,21 @@ public class ConnectorScheduler {
 			while (!executionQueue.isEmpty()) {
 				Connector c = executionQueue.remove();
 				int retreived = c.checkNewMessages(RETREIVE_MESSAGES_LIMIT);
-				if (retreived == RETREIVE_MESSAGES_LIMIT) {
-					executionQueue.add(c);
-				}
+				logger.info("Retrieved {} messages.", retreived);
+			}
+			try {
+				TimeUnit.SECONDS.sleep(20);
+			} catch (InterruptedException e) {
 			}
 		}
 	}
-	
+
+	public synchronized void bindConnector(Connector c) {
+		scheduledConnectors.add(c);
+	}
+
+	public synchronized void unbindConnector(Connector c) {
+		scheduledConnectors.remove(c);
+	}
+
 }
